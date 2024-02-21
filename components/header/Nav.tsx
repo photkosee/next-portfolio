5c76ee1fdb81bdf5c3cd5833f27b685ac17ff93f
Phot Koseekrainiramon <kosee.phot@gmail.com>
2024-02-21 12:21:09 +1100

    add: pinging project nav for first-time users to get their attention

--- components/header/Nav.tsx
+++ components/header/Nav.tsx
@@ -2,6 +2,8 @@
 
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
+
 import { motion } from "framer-motion";
 
 const links = [
@@ -32,7 +34,19 @@ const Nav = ({
   linkStyles: string;
   underlineStyles: string;
 }) => {
+  const [hasVisitedProject, setHasVisitedProject] = useState<boolean>(false);
   const path = usePathname();
+  // Check whether the user have visited the project page before
+  // If not, show the ping animation
+  const checkPing = (linkPath: string) => {
+    return linkPath === "/projects" && path !== linkPath && !hasVisitedProject;
+  };
+
+  useEffect(() => {
+    if (path === "/projects") {
+      setHasVisitedProject(true);
+    }
+  }, [path]);
 
   return (
     <nav
@@ -43,12 +57,22 @@ const Nav = ({
       {links.map((link, index) => (
         <Link
           className={`
-          capitalize ${linkStyles} flex
+          capitalize ${linkStyles} flex relative
           `}
           href={link.path}
           key={index}
-          target={link.name === "my blog" ? "_blank" : ""}
+          target={link.name === "blog" ? "_blank" : ""}
         >
+          <span
+            className={`${
+              !checkPing(link.path) && "hidden"
+            } animate-ping absolute -right-3 inline-flex h-2 w-2 rounded-full bg-primary opacity-75`}
+          />
+          <span
+            className={`${
+              !checkPing(link.path) && "hidden"
+            } absolute -right-3 inline-flex rounded-full h-2 w-2 bg-primary`}
+          />
           {link.path === path && (
             <motion.span
               className={`${underlineStyles}`}
